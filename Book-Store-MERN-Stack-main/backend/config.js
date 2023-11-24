0bd13ff5abe7c08af771f4000f59d798ab09abc1
prince-vasoya <prince1065.be21@chitkara.edu.in>
2023-11-24 10:44:50 +0530

    first comit

---
+++ Book-Store-MERN-Stack-main/backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://admin:9518139625@cluster0.nzldfl3.mongodb.net/bookStore?retryWrites=true&w=majority';
---
+++ Book-Store-MERN-Stack-main/backend/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
+
+const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
+
+app.use(cors());
+
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
+
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send('Welcome To MERN Stack Tutorial');
+});
+
+app.use('/books', booksRoute);
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
